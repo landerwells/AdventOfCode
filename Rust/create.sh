921498c268137da20c18752d2c909a797e4514fa
landerwells <landerwells@gmail.com>
2024-02-27 16:20:43 -0500

    modified create file so that it checks before making file

--- Rust/create.sh
+++ Rust/create.sh
@@ -1,38 +1,37 @@
 #!/bin/sh
 
-# The script expects one argument: the day number
+# The script expects two arguments: the year and the day number
 year=$1
 day_number=$2
 
 # Use printf to format the day number with leading zeros
 formatted_day=$(printf "day%02d" "$day_number")
 
+# Define the file path for the destination file
+destination_file="src/aoc$1/$formatted_day.rs"
 
-# copy the template file into the correct year and name it the correct day, 
-# and modify the the contents to match
+# Check if the file already exists
+if [ -f "$destination_file" ]; then
+    echo "File '$destination_file' already exists."
+else
+    # If the file doesn't exist, copy the template file into the correct year and name it the correct day,
+    # and modify the contents to match
 
-mkdir -p src/aoc$1
-cp src/template.rs src/aoc$1/$formatted_day.rs
+    mkdir -p "src/aoc$year"
+    cp src/template.rs "$destination_file"
 
+    # Modify the contents of the copied file
+    sed -i '' "s/get_daily_input()/get_daily_input($day_number, $year)/" "$destination_file"
 
-# sed 's/get_daily_input\(\)/get_daily_input\(day_number, year\)/' src/aoc$1/$formatted_day
-sed -i '' "s/get_daily_input()/get_daily_input($day_number, $year)/" src/aoc$year/$formatted_day.rs
+    # Append the new module to the mod.rs file
+    mod_file="src/aoc$year/mod.rs"
 
+    # Add new mod declaration
+    echo "pub mod $formatted_day;" >> "$mod_file"
 
-# the last things to do are modify the mod file in the specific year and the main file
-
-
-# Append the new module to the mod.rs file
-mod_file="src/aoc$year/mod.rs"
-
-# Add new mod declaration
-echo "pub mod $formatted_day;" >> $mod_file
-
-# Add the day's run function call in the run_all function. This part is a bit tricky
-# because we need to insert the line before the last brace of the run_all function.
-# We can use sed to do this.
-
-sed -i '' "/run_all()/ a\\
+    # Add the day's run function call in the run_all function.
+    # We can use sed to do this.
+    sed -i '' "/run_all()/ a\\
     $formatted_day::run();\\
-" $mod_file
-
+    " "$mod_file"
+fi
